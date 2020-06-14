d19a82b6ee5a4d202ca94ac415d0ea096e7e6ae6
Jiseop Han <jshan2017@gmail.com>
2020-06-15 00:43:45 +0900

    designed basic directory structure

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.scss";
+import Shell from "./Shell";
+
+function App() {
+  return (
+    <div className="App">
+      <Shell />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Shell.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Shell.scss";
+
+const Shell = () => {
+  return (
+    <div className="shell">
+      <p>$ Hello tabash!</p>
+    </div>
+  );
+};
+
+export default Shell;
