27508907675ea579d1846ac21a14c6fa452fba75
Jiseop Han <jshan2017@gmail.com>
2020-06-15 22:22:07 +0900

    implement repetive input

--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/Shell.scss";
 
 const Shell = () => {
+  const [intputValue, setInputValue] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [consoleHistory, setConsoleHistory] = useState<string[]>([]);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
   return (
-    <div className="shell">
-      <p>$ Hello tabash!</p>
+    <div className="shell" onClick={() => inputRef.current?.focus()}>
+      {consoleHistory.map((msg, index) => {
+        return <p key={index}>{msg}</p>;
+      })}
+      <input
+        ref={inputRef}
+        value={`$  ${intputValue}`}
+        onChange={(e) => setInputValue(e.target.value.substring(3))}
+        onKeyDown={(e) => {
+          if (e.key !== "Enter") {
+            return;
+          }
+          setConsoleHistory([...consoleHistory, intputValue]);
+          setInputValue("");
+        }}
+      />
     </div>
   );
 };
