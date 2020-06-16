4493d8d999dd40f9e1a5807d992c5c85bd7cd94d
Jiseop Han <jshan2017@gmail.com>
2020-06-16 22:35:08 +0900

    add link to header , change color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "../styles/Header.scss";
+import githubLogo from "../assets/GitHub-Mark-32px.png";
 const Header = () => {
-  return <div className="app-header">tabash</div>;
+  return (
+    <div className="app-header">
+      tabash
+      <a href="https://github.com/jshan2017/tabash">
+        <img src={githubLogo} alt="visit github" />
+      </a>
+    </div>
+  );
 };
 
 export default Header;
