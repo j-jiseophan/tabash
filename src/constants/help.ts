64634a1f685f75462cf68495ca9d36d32eda6728
Jiseop Han <jshan2017@gmail.com>
2020-06-19 23:07:12 +0900

    change emoji

--- src/constants/help.ts
+++ src/constants/help.ts
@@ -4,7 +4,7 @@ export const HELP_MSGS = [
   `🪐 tabash ${version}`,
   `a bash-like 'new tab' page for browser.`,
   `Visit https://github.com/jshan2017/tabash for details.`,
-  `📖`,
+  `📕`,
   `ls # list registered  links`,
   `[key]=[url] # list [url] with [key]`,
   `[key] # open link`,
