3d7a0022fbb9553eb79930e42fff9995cea456cd
Jiseop Han <jiseop.han@voyagerx.com>
2022-02-06 14:21:17 +0900

    Revert "fix flickering"
    This reverts commit b8ddd87fffa8c73908d549b28a7bcaa808f625f7.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,19 +3,8 @@ import "../styles/App.scss";
 import Header from "./Header";
 import ShellWrapper from "./ShellWrapper";
 import { ThemeProvider } from "../contexts/theme";
-import { parseQueryString } from "../utils/utils";
 
 function App() {
-  const queryParams = parseQueryString();
-  const focused = queryParams["focus"];
-  if (!focused) {
-    chrome.tabs.create({
-      url: chrome.extension.getURL("/index.html?focus=true"),
-    });
-    window.close();
-    return null;
-  }
-  
   return (
     <div className="App">
       <ThemeProvider>
