7e753602f949fd0d1b5e15ff6b4a7a284a39305c
Jiseop Han <jshan2017@gmail.com>
2026-01-15 00:22:15 +0900

    디자인 개선

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import classnames from "classnames";
 import "../styles/Header.scss";
 import githubLogo from "../assets/GitHub-Mark-32px.png";
 import DarkModeButton from "./DarkModeButton";
+import { ThemeContext } from "../contexts/theme";
+
 const Header = () => {
+  const { isDarkMode } = useContext(ThemeContext);
+
   return (
-    <div className="app-header">
-      <span className="title"> 🪐 tabash</span>
+    <div className={classnames("app-header", { light: !isDarkMode })}>
+      <div className="window-buttons">
+        <span className="close"></span>
+        <span className="minimize"></span>
+        <span className="maximize"></span>
+      </div>
+      <span className="title">tabash</span>
       <div className="header-buttons">
         <DarkModeButton />
         <a href="https://github.com/jshan2017/tabash">
