4449a2b06822c3ed719e30a9a14c382ac96b26aa
Jiseop Han <jshan2017@gmail.com>
2026-01-16 16:15:06 +0900

    배경화면 다크/라이트 다르게 지원

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import { useContext } from "react";
+import classnames from "classnames";
 import "../styles/App.scss";
 import Header from "./Header";
 import ShellWrapper from "./ShellWrapper";
-import { ThemeProvider } from "../contexts/theme";
+import { ThemeProvider, ThemeContext } from "../contexts/theme";
+
+function AppContent() {
+  const { isDarkMode } = useContext(ThemeContext);
+
+  return (
+    <div className={classnames("App", { light: !isDarkMode })}>
+      <Header />
+      <ShellWrapper />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <ThemeProvider>
-        <Header />
-        <ShellWrapper />
-      </ThemeProvider>
-    </div>
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
 
